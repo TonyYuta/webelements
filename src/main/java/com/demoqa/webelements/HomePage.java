125e4001ae32abf63853bb4e20a28f9640cbe62d
TonyYuta <tonykagaya@gmail.com>
2018-03-05 13:15:08 -0800

    init

---
+++ src/main/java/com/demoqa/webelements/HomePage.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: HomePage.java<br>
+ *
+ *   Yutaka<br>
+ *   Created: Mar 5, 2018
+ *   
+ */
+
+package com.demoqa.webelements;
+
+
+/**
+ * HomePage //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Yutaka
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class HomePage {
+	
+	
+}
---
+++ src/test/java/com/demoqa/webelements/HomePageTest.java
@@ -0,0 +1,34 @@
+/**
+ *   File Name: HomePageTest.java<br>
+ *
+ *   Yutaka<br>
+ *   Created: Mar 5, 2018
+ *   
+ */
+
+package com.demoqa.webelements;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * HomePageTest //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Yutaka
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class HomePageTest {
+	
+	@Test
+	public void qqq() {
+		AssertJUnit.assertEquals("2143242352", "324325352");
+	}
+}
