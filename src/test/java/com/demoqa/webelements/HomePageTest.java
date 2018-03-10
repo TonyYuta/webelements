9ecd6a1b8de75a874fbb5e6d701277109b41c23a
TonyYuta <tonykagaya@gmail.com>
2018-03-10 12:25:43 -0800

    Jenkinsfile, xml mod

--- src/test/java/com/demoqa/webelements/HomePageTest.java
+++ src/test/java/com/demoqa/webelements/HomePageTest.java
@@ -28,98 +28,98 @@ public class HomePageTest extends BaseTest {
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "RegistrationPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "RegistrationPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToRegistrationPage() {
 		String expected = "Registration | Demoqa";
 		homePage.navigateToRegistrationPage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "DraggablePage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "DraggablePage",  "regression", "all"}, priority = 0)
 	public void testNavigateToDraggablePage() {
 		String expected = "Draggable | Demoqa";
 		homePage.navigateToDraggablePage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "DroppablePage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "DroppablePage",  "regression", "all"}, priority = 0)
 	public void testNavigateToDroppablePage() {
 		String expected = "Droppable | Demoqa";
 		homePage.navigateToDroppablePage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "ResizablePage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "ResizablePage",  "regression", "all"}, priority = 0)
 	public void testNavigateToResizablePage() {
 		String expected = "Resizable | Demoqa";
 		homePage.navigateToResizablePage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "SelectablePage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "SelectablePage",  "regression", "all"}, priority = 0)
 	public void testNavigateToSelectablePage() {
 		String expected = "Selectable | Demoqa";
 		homePage.navigateToSelectablePage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "SortablePage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "SortablePage",  "regression", "all"}, priority = 0)
 	public void testNavigateToSortablePage() {
 		String expected = "Sortable | Demoqa";
 		homePage.navigateToSortablePage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 		
-	@Test(enabled = true, groups = {"Homepage", "AccordionPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "AccordionPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToAccordionPage() {
 		String expected = "Accordion | Demoqa";
 		homePage.navigateToAccordionPage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "AutocompletePage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "AutocompletePage",  "regression", "all"}, priority = 0)
 	public void testNavigateToAutocompletePage() {
 		String expected = "Autocomplete | Demoqa";
 		homePage.navigateToAutocompletePage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "DatepickerPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "DatepickerPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToDatepickerPage() {
 		String expected = "Datepicker | Demoqa";
 		homePage.navigateToDatepickerPage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "MenuPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "MenuPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToMenuPage() {
 		String expected = "Menu | Demoqa";
 		homePage.navigateToMenuPage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "SliderPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "SliderPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToSliderPage() {
 		String expected = "Slider | Demoqa";
 		homePage.navigateToSliderPage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "TabsPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "TabsPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToTabsPage() {
 		String expected = "Tabs | Demoqa";
 		homePage.navigateToTabsPage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "TooltipPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "TooltipPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToTooltipPage() {
 		String expected = "Tooltip | Demoqa";
 		homePage.navigateToTooltipPage();
 		Assert.assertEquals(homePage.currentPageTitle(), expected, "URI doesn't match to Home page");
 	}
 	
-	@Test(enabled = true, groups = {"Homepage", "FramesAndWindowsPage",  "bat", "regression", "all"}, priority = 0)
+	@Test(enabled = true, groups = {"Homepage", "FramesAndWindowsPage",  "regression", "all"}, priority = 0)
 	public void testNavigateToFramesAndWindowsPage() {
 		String expected = "Frames and windows | Demoqa";
 		homePage.navigateToFramesAndWindowsPage();
